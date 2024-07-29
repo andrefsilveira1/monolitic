591f85db358033b4f462183fb0394d6fe18b39cb
André Freitas <freitasandre38@gmail.com>
2024-07-29 19:44:32 -0300

    feat: create product base entity

--- src/modules/@shared/domain/entity/base.entity.ts
+++ src/modules/@shared/domain/entity/base.entity.ts
@@ -1,24 +1,26 @@
+import Id from "../../value-object/id.value-object";
+
 export default class BaseEntity {
-    private _id: string;
+    private _id: Id;
     private _createdAt: Date;
     private _updatedAt: Date;
 
-    constructor(id: string) {
+    constructor(id: Id) {
         this._id = id;
         this._createdAt = new Date();
         this._updatedAt = new Date();
     }
 
-    get id(): string {
+    get id(): Id {
         return this._id;
     }
 
-    get createdAt(): string {
-        return this.createdAt;
+    get createdAt(): Date {
+        return this._createdAt;
     }
 
-    get updatedAt(): string {
-        return this.updatedAt;
+    get updatedAt(): Date {
+        return this._updatedAt;
     }
 
     set updatedAt(updatedAt: Date) {
