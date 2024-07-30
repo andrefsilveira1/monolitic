320eef63a214796aaa6a8221bd79c5e88326e555
André Freitas <freitasandre38@gmail.com>
2024-07-30 09:35:02 -0300

    chore: finish product repository test

--- src/modules/product-adm/repository/product.repository.spec.ts
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import  ProductModel  from "./product.model";
+import Product from "../domain/product.entity";
+import Id from "../../@shared/value-object/id.value-object";
 
 describe("Product repository test", () => {
     let sequelize: Sequelize;
@@ -13,5 +15,34 @@ describe("Product repository test", () => {
 
         await sequelize.addModels([ProductModel]);
         await sequelize.sync();
-    })
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("Should create a product", async() => {
+        const productRepository = new ProductRepository();
+        
+        const productProps = {
+            id: new Id("1"),
+            name: "Product 1",
+            description: "description",
+            purchasePrice: 100,
+            stock: 10,
+        }
+        const product = new Product(productProps);
+        const result = await productRepository.add(product);
+
+        const productDb = await ProductModel.findOne({
+            where: {id: productProps.id.id},
+        });
+
+        expect(productProps.id.id).toEqual(productDb?.id);
+        expect(productProps.name).toEqual(productDb?.name);
+        expect(productProps.description).toEqual(productDb?.description);
+        expect(productProps.purchasePrice).toEqual(productDb?.purchasePrice);
+        expect(productProps.stock).toEqual(productDb?.stock);
+
+    });
 });
