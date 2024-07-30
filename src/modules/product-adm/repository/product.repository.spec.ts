1b9405538b5cdaf286a948bc6fb6b3561b9bc844
André Freitas <freitasandre38@gmail.com>
2024-07-30 09:26:11 -0300

    feat: create product repository and test product repository

---
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize-typescript";
+import  ProductModel  from "./product.model";
+
+describe("Product repository test", () => {
+    let sequelize: Sequelize;
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true}
+        });
+
+        await sequelize.addModels([ProductModel]);
+        await sequelize.sync();
+    })
+});
