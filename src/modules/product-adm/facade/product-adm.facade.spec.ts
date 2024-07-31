c083b5101c209cca8d37e4f9e2777c4e46b61dc8
André Freitas <freitasandre38@gmail.com>
2024-07-31 20:50:31 -0300

    feat: create product adm factory

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -3,6 +3,7 @@ import ProductModel from "../repository/product.model";
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add-product/add-product.usecase";
 import ProductAdmFacade from "./product-adm.facade";
+import ProductAdmFactory from "../factory/facade.factory";
 
 describe("Product adm facade", () => {
     let sequelize: Sequelize;
@@ -23,13 +24,7 @@ describe("Product adm facade", () => {
     });
 
     it("Should create a product", async () => {
-        const productRepository = new ProductRepository();
-        const addProductUseCase = new AddProductUseCase(productRepository);
-        const productFacade = new ProductAdmFacade({
-            addUseCase: addProductUseCase,
-            stockUseCase: undefined,
-        });
-
+        const productFacade = ProductAdmFactory.create();
         const input = {
             id: "1",
             name: "product",
