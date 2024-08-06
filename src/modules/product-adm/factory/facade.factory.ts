637ab4dacab54850c496c596789c70415d7c3c06
André Freitas <freitasandre38@gmail.com>
2024-08-06 20:20:38 -0300

    refactor: recreate check stock use case, use imports from facade

--- src/modules/product-adm/factory/facade.factory.ts
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -1,6 +1,7 @@
 import ProductAdmFacade from "../facade/product-adm.facade";
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+import CheckStockUseCase from "../usecase/check-stock/check-stock.usecase";
 
 export default class ProductAdmFactory {
     static create() {
