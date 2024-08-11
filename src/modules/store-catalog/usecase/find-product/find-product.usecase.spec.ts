b6fcf1bcbfcca6c3b7ae32cfe4feb87a76f251c4
André Freitas <freitasandre38@gmail.com>
2024-08-11 09:43:10 -0300

    feat: create find product use case

---
+++ src/modules/store-catalog/usecase/find-product/find-product.usecase.spec.ts
@@ -0,0 +1,36 @@
+import Id from "../../../@shared/value-object/id.value-object";
+import Product from "../../domain/product.entity";
+import FindAllProductsUseCse from "../find-all-products/find-all-products-usecase";
+
+const product = new Product({
+    id: new Id("1"),
+    name: "product",
+    description: "description",
+    salesPrice: 100,
+});
+
+const MockRepository = () => {
+    return {
+        findAll: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(product))
+    }
+}
+
+describe("Find a product usecase unit test", () => {
+    it("Should find a product", async () => {
+        const productRepository = MockRepository()
+        const usecase = new FindProductUseCse(productRepository);
+
+        const input = {
+            id: '1',
+        };
+
+        const result = await usecase.execute(input);
+
+        expect(productRepository.find).toHaveBeenCalled();
+        expect(result.id.id).toBe("1");
+        expect(result.name).toBe("product");
+        expect(result.description).toBe("description");
+        expect(result.salesPrice).toBe(100);
+    })
+})
