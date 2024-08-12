2bc9e4f0f4da374056bf54e6ac33a943d16baa04
André Freitas <freitasandre38@gmail.com>
2024-08-12 09:59:41 -0300

    feat: implements add client use case

---
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.spec.ts
@@ -0,0 +1,28 @@
+import AddClientUseCase from "./add-client.usecase";
+
+const MockRepository = () => {
+    return {
+        add: jest.fn(),
+        find: jest.fn(),
+    }
+}
+
+describe("Add Client usecase unit test", () => {
+    it("Should add new client", async () => {
+        const repository = MockRepository();
+        const usecase = new AddClientUseCase(repository);
+        const input = {
+            id: "1",
+            name: "client",
+            email: "client@gmail.com",
+            address: "address"
+        };
+        
+        const result = await usecase.execute(input);
+        expect(repository.add).toHaveBeenCalled();
+        expect(result.id).toEqual(input.id);
+        expect(result.name).toEqual(input.name);
+        expect(result.email).toEqual(input.email);
+        expect(result.address).toEqual(input.address);
+    })
+})
