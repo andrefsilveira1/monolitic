115c4bbb0325eab9b1941cc9e01fde480aab098c
André Freitas <freitasandre38@gmail.com>
2024-08-14 20:32:18 -0300

    feat: create process payment usecase test

---
+++ src/modules/payment/usecase/process-payment/process-payment.usecase.spec.ts
@@ -0,0 +1,35 @@
+import Id from "../../../@shared/value-object/id.value-object";
+import Transaction from "../../domain/transaction";
+import ProcessPaymentUseCase from "./process-payment.usecase";
+
+const transaction = new Transaction({
+    id: new Id('1'),
+    amount: 90,
+    orderId: '1',
+});
+
+const MockRepository = () => {
+    return {
+        save: jest.fn().mockReturnValue(Promise.resolve(transaction)),
+    }
+}
+
+describe("Process payment usecase unit test", () => {
+    it("Should approve a transaction", async () => {
+        const paymentRepository = MockRepository();
+        const usecase = new ProcessPaymentUseCase(paymentRepository);
+        const input = {
+            orderId: "1",
+            amount: 100
+        };
+
+        const result = await usecase.execute(input);
+        expect(result.transactionId).toBe(transaction.id.id);
+        expect(paymentRepository.save).toHaveBeenCalled();
+        expect(result.status).toBe("approved");
+        expect(result.amount).toBe(90);
+        expect(result.orderId).toBe('1');
+        expect(result.createdAt).toBe(transaction.createdAt);
+        expect(result.updatedAt).toBe(transaction.updatedAt);
+    })
+})
