3b25c09395b87e5682c5d903f18e19abaf8ff63c
André Freitas <freitasandre38@gmail.com>
2024-08-27 21:15:53 -0300

    chore: remove logs

--- src/api/routes/checkout.ts
+++ src/api/routes/checkout.ts
@@ -1,12 +1,4 @@
 import express, {Request, Response} from "express";
-import ProductAdmFactory from "../../modules/product-adm/factory/facade.factory";
-import ClientAdmFacadeFactory from "../../modules/client-adm/factory/client-adm.facade.factory";
-import CheckStockUseCase from "../../modules/product-adm/usecase/check-stock/check-stock.usecase";
-import PlaceOrderUseCase from "../../modules/checkout/usecase/place-order/place-order.usecase";
-import ClientAdmFacade from "../../modules/client-adm/facade/client-adm.facade";
-import StoreCatalogFactory from "../../modules/store-catalog/factory/facade.factory";
-import InvoiceFacadeFactory from "../../modules/invoice/factory/invoice.facade.factory";
-import PaymentFacadeFactory from "../../modules/payment/factory/payment.facade.factory";
 import CheckoutFactory from "../../modules/checkout/factory/checkout.factory";
 
 export const checkoutRoute = express.Router();
@@ -19,9 +11,7 @@ checkoutRoute.post("/", async (req: Request, res: Response) => {
             clientId: req.body.clientId,
             products: req.body.products,
         }
-        console.log("PLACE ORDER INPUT ====>", placeOrderInputDto)
         const order = await placeOrderUseCase.execute(placeOrderInputDto)
-
         res.send(order);
     } catch (err) {
         res.status(500).send(err);
--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -41,15 +41,11 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
         if (!client) {
             throw new Error("Client not found");
         }
-        console.log("HIT CLIENT ====>", client)
 
         await this.validateProducts(input);
-        console.log("HIT PROJECT ====>", input)
         const products = await Promise.all(
             input.products.map((p) => this.getProduct(p.productId))
         );
-
-        console.log("HIT PROJECT ====>", products)
 
         const address = new Address({
             street: client.address.street,
@@ -60,9 +56,6 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
             zipCode: client.address.zipCode,
         });
 
-        console.log("HIT address ===>", address)
-
-
         const myClient = new Client({
             id: new Id(client.id),
             name: client.name,
@@ -71,14 +64,10 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
             document: client.document
         });
 
-        console.log("HIT myClient ===>", myClient)
-
         const order = new Order({
             client: myClient,
             products
         });
-
-        console.log("HIT ORDER ===>", order)
 
         const payment = await this._paymentFacade.process({
             orderId: order.id.id,
@@ -137,9 +126,7 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
     }
 
     private async getProduct(productId: string): Promise<Product> {
-        console.log("LOG ON productId productId ===>", productId);
         const product = await this._catalogFacade.find({ id: productId });
-        console.log("LOG ON GET PRODUCTS ===>", product);
 
         if (!product) {
             throw new Error("Product not found");
